5811587a57d37379a5c36b0caff409541ef92734
Abhishek Kumar <93286089+gwydion67@users.noreply.github.com>
2025-02-13 16:11:33 +0530

    fix ts config

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-import { loadEnv } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080"
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {

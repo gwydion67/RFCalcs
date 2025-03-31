0731303dc41ae8e1a42d73758e073502e2233df3
Abhishek Kumar <gwydionmaranio@gmail.com>
2025-04-01 00:41:10 +0530

    fix analysis result not loading

--- Backend/src/heath/health.ts
+++ Backend/src/heath/health.ts
@@ -3,6 +3,8 @@ import { api } from "encore.dev/api";
 export const awake = api(
   { method: "GET", path: "/health/awake", expose: true },
   async (): Promise<Response> => {
+    // This is a simple health check endpoint
+
     return {
       message: "hello i am awake"
     }
